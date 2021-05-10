094becbd48da4d66a10bd06036c5586d3280aaa2
wenajote989 <fe.astorgav@duocuc.cl>
2021-05-09 20:32:45 -0400

    UPDATEMAX

---
+++ js/feriados.js
@@ -0,0 +1,12 @@
+
+let url = "https://apis.digital.gob.cl/fl/feriados"
+$.get(url, function(respuesta){
+
+   /* respuesta.forEach(function(item) {
+        console.log(item)
+    });*/
+    let feriado = respuesta[respuesta.length -15]
+
+    $("#feriado").text(feriado.nombre + " - "
+    + feriado.fecha)
+})

39358d1bcb73e114445609c87467af794fa85be3
Jeremaia <xyerkox99@gmail.com>
2021-05-31 07:35:09 -0400

    rework

---
+++ Proyectos django/TallerMecanico/core/views.py
@@ -0,0 +1,8 @@
+from django import http
+from django.http.response import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'core/index.html')

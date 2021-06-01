134c32f844b4a489ff14e08c49f02efe6bbb82b7
wenajote989 <fe.astorgav@duocuc.cl>
2021-06-01 17:04:01 -0400

    fotosok

--- dios/TallerMecanico/core/urls.py
+++ dios/TallerMecanico/core/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
-from .views import index
+from .views import index, galeria, buscador, categoria1, chevrolet, ford, inicio_sesion, mazda, mecanico1, raptor, registrar, solicitud
+
 
 urlpatterns = [
     path('', index, name="index"),
+    path('galeria/', galeria, name="galeria"),
+    path('buscador/', buscador, name="buscador"),
+    path('categoria1/', categoria1, name="categoria1"),
+    path('chevrolet/', chevrolet, name="chevrolet"),
+    path('ford/', ford, name="ford"),
+    path('inicio_sesion/', inicio_sesion, name="inicio_sesion"),
+    path('mazda/', mazda, name="mazda"),
+    path('mecanico1/', mecanico1, name="mecanico1"),
+    path('raptor/', raptor, name="raptor"),
+    path('registrar/', registrar, name="registrar"),
+    path('solicitud/', solicitud, name="solicitud"),
 ]
--- dios/TallerMecanico/core/views.py
+++ dios/TallerMecanico/core/views.py
@@ -6,3 +6,25 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'core/index.html')
+def galeria(request):
+    return render(request, 'core/galeria.html')
+def buscador(request):
+    return render(request, 'core/buscador.html')
+def categoria1(request):
+    return render(request, 'core/categoria1.html')
+def chevrolet(request):
+    return render(request, 'core/chevrolet.html')
+def ford(request):
+    return render(request, 'core/ford.html')
+def inicio_sesion(request):
+    return render(request, 'core/inicio_sesion.html')
+def mazda(request):
+    return render(request, 'core/mazda.html')
+def mecanico1(request):
+    return render(request, 'core/mecanico1.html')
+def raptor(request):
+    return render(request, 'core/raptor.html')
+def registrar(request):
+    return render(request, 'core/registrar.html')
+def solicitud(request):
+    return render(request, 'core/solicitud.html')

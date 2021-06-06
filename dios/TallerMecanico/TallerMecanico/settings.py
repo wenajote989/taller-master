543772a1609ae659575a98ffacdffc66c9e8a9f1
Jeremaia <xyerkox99@gmail.com>
2021-06-06 14:09:03 -0400

    models

--- dios/TallerMecanico/TallerMecanico/settings.py
+++ dios/TallerMecanico/TallerMecanico/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'TallerMecanico.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': '127.0.0.1:1521/xe',
+        'USER': 'c##tallermec',
+        'PASSWORD': 'taller',
+        'TEST': {
+            'USER': 'default_test',
+            'TBLSPACE': 'default_test_tbls',
+            'TBLSPACE_TMP': 'default_test_tbls_tmp',
+        }
     }
 }
 

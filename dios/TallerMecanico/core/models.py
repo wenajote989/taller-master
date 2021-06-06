bf2ada5c439e93337698cda87a5057be90877165
Jeremaia <xyerkox99@gmail.com>
2021-06-06 14:40:56 -0400

    fix models

--- dios/TallerMecanico/core/models.py
+++ dios/TallerMecanico/core/models.py
@@ -4,39 +4,39 @@ from django.db import models
 
 # Mecanico
 
-class Mecanico(models.Model):
-    nombre_mec = models.CharField(max_length=50)
-    apellido_sol = models.CharField(max_length=20)
-    descr_mec = models.TextField
-    img_mec = models.ImageField
+#class Mecanico(models.Model):
+#    nombre_mec = models.CharField(max_length=50)
+#    apellido_sol = models.CharField(max_length=20)
+#    descr_mec = models.TextField
+#    img_mec = models.ImageField
 
 # Catergoria
 
-class Categoria(models.Model):
-    nombre_cat = models.CharField(max_length=20)
+#class Categoria(models.Model):
+#    nombre_cat = models.CharField(max_length=20)
 
 # Auto
 
-class Auto(models.Model):
-    nombre_auto = models.CharField(max_length=20)
-    modelo_auto = models.CharField(max_length=15)
-    patente_auto = models.CharField(max_length=10)
-    probl_auto = models.TextField
-    rep_auto = models.TextField
-    img_auto = models.ImageField
+#class Auto(models.Model):
+#    nombre_auto = models.CharField(max_length=20)
+#    modelo_auto = models.CharField(max_length=15)
+#    patente_auto = models.CharField(max_length=10)
+#    probl_auto = models.TextField
+#    rep_auto = models.TextField
+#    img_auto = models.ImageField
 
 # Solicitud
 
-class Solicitud(models.Model):
-    nombre_sol = models.CharField(max_length=20)
-    apellido_sol = models.CharField(max_length=20)
-    modelo_sol = models.CharField(max_length=15)
-    patente_sol = models.CharField(max_length=10)
-    mail_sol = models.EmailField
+#class Solicitud(models.Model):
+#    nombre_sol = models.CharField(max_length=20)
+#    apellido_sol = models.CharField(max_length=20)
+#    modelo_sol = models.CharField(max_length=15)
+#    patente_sol = models.CharField(max_length=10)
+#    mail_sol = models.EmailField
 
 # Usuario
 
-class Usuario(models.Model):
-    nombre_user = models.CharField(max_length=20)
-    contra_user = models.CharField(max_length=20)
-    mail_user= models.EmailField
+#class Usuario(models.Model):
+#    nombre_user = models.CharField(max_length=20)
+#    contra_user = models.CharField(max_length=20)
+#    mail_user= models.EmailField

9ed51318c8bbeb5db3817f167491c29e6417b45d
Jeremaia <37198184+Jeremaia@users.noreply.github.com>
2021-06-14 19:13:52 -0400

    Add files via upload

--- dios/TallerMecanico/core/static/core/js/feriados.js
+++ dios/TallerMecanico/core/static/core/js/feriados.js
@@ -5,8 +5,18 @@ $.get(url, function(respuesta){
    /* respuesta.forEach(function(item) {
         console.log(item)
     });*/
-    let feriado = respuesta[respuesta.length -15]
-    
+    var hoy = new Date()
+    var fecha = Date(hoy.getFullYear()+'-'+(hoy.getMonth()+1)+'-'+hoy.getDate())
+    var i = 0
+
+    while (i < 21) {
+        feriado = respuesta[respuesta.length - 14 + i]
+        if (Date(feriado.fecha) < fecha){
+            i = i + 1
+        }else {
+            i = 22
+        }
+    }
 
     $("#feriado").text(feriado.nombre + " - "
     + feriado.fecha)
